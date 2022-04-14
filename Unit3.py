8660210d791dd7ed6a991b6e7fc791366fdfe8d9
Fan4_Metal <fan4_metal@mail.ru>
2022-04-14 13:25:15 +0300

    Second commit

--- Unit3.py
+++ Unit3.py
@@ -1,19 +1,43 @@
 import os
 from delphivcl import *
+from kinopoisk.movie import Movie
 
 class Form3(Form):
 
     def __init__(self, owner):
-        self.Label1 = None
-        self.Label2 = None
-        self.Edit1 = None
-        self.Edit2 = None
-        self.Button1 = None
-        self.Button2 = None
-        self.ListBox1 = None
-        self.Button3 = None
-        self.Image1 = None
-        self.LoadProps(os.path.join(os.path.dirname(os.path.abspath(__file__)), "Unit3.pydfm"))
+        self.LoadProps(os.path.join(os.path.dirname(os.path.abspath(__file__)), "KinolistGUI.pydfm"))
+        self.Caption = "Поиск фильмов"
+        self.Label1 = Label(self)
+        self.Label1.Caption = "Поиск"
+        self.Label2 = Label(self)
+        self.Label2.Caption = "Результат"
+                
+        self.Edit1 = Edit(self)
+        self.Edit1.Text = ""
+        self.Edit2 = Edit(self)
+        self.Edit2. Text = ""
+        self.Button1 = Button(self)
+        self.Button1.Caption = "Поиск"
+        self.Button1.OnClick = self.button1_click
+        
+        self.Button2 = Button(self)
+        self.Button1.Caption = "Сохранить   "
+        # self.Button1.OnClick = self.button2_click
+        
+        self.ListBox1 = ListBox(self)
+        self.Button3 = Button(self)
+        self.Image1 = Picture(self)
+
+    def button1_click(self, Sender):
+        search = self.Edit1.Text
+        movie_list = Movie.objects.search(search)
+        self.Edit2.Text = movie_list[0]
+        # print(type(movie_list))
+    
+    # def button2_click(self, Sender):
+    #     self.ListBox1.
+        
+
 
 def main():
     Application.Initialize()
